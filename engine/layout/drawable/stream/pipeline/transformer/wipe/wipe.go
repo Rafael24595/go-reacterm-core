78b2dcc45d006997593146bf4c1e2138f0aad7f5
Rafael Ruiz <rafaelmostoles24595@gmail.com>
2026-05-08 01:20:42 +0200

    chore: move dran, focus and wipe to transformer

--- engine/layout/drawable/stream/pipeline/transformer/wipe/wipe.go
+++ engine/layout/drawable/stream/pipeline/transformer/wipe/wipe.go
@@ -4,11 +4,22 @@ import (
 	"github.com/Rafael24595/go-reacterm-core/engine/layout/drawable"
 	"github.com/Rafael24595/go-reacterm-core/engine/layout/drawable/stream/pipeline"
 	"github.com/Rafael24595/go-reacterm-core/engine/model/winsize"
+	"github.com/Rafael24595/go-reacterm-core/engine/render/text"
 )
 
 func InitTransformer() pipeline.InitTransformer {
 	return func(size winsize.Winsize, drw drawable.Drawable) drawable.Drawable {
 		drw.Wipe()
 		return drw
+	}
+}
+
+func DrawTransformer() pipeline.DrawTransformer {
+	return func(size winsize.Winsize, drw drawable.Drawable) ([]text.Line, bool) {
+		lines, status := drw.Draw(size)
+		if !status {
+			drw.Wipe()
+		}
+		return lines, true
 	}
 }
