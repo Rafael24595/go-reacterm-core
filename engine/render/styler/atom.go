0cf2290061853255136d7c00fefd139df4309acf
Rafael Ruiz <rafaelmostoles24595@gmail.com>
2026-05-19 21:02:56 +0200

    feat: add core styler infrastructure

---
+++ engine/render/styler/atom.go
@@ -0,0 +1,68 @@
+package styler
+
+import (
+	"strings"
+
+	"github.com/Rafael24595/go-reacterm-core/engine/commons/structure/dict"
+	"github.com/Rafael24595/go-reacterm-core/engine/render/style"
+)
+
+type AtomStyler func(string) string
+
+func pa(k style.Atom, s AtomStyler) dict.Pair[style.Atom, AtomStyler] {
+	return dict.NewPair(k, s)
+}
+
+var DefaultAtomStyler = dict.NewInmutableLinkedMap(
+	pa(style.AtmLower, func(text string) string {
+		return strings.ToLower(text)
+	}),
+	pa(style.AtmUpper, func(text string) string {
+		return strings.ToUpper(text)
+	}),
+	pa(style.AtmBold, func(text string) string {
+		return text
+	}),
+	pa(style.AtmSelect, func(text string) string {
+		return text
+	}),
+)
+
+type Atom struct {
+	table *dict.LinkedMap[style.Atom, AtomStyler]
+}
+
+func NewAtom() *Atom {
+	instance := &Atom{}
+	return instance.lazyInit()
+}
+
+func (a *Atom) lazyInit() *Atom {
+	if a.table != nil {
+		return a
+	}
+
+	a.table = dict.NewLinkedMap[style.Atom, AtomStyler]()
+	return a
+}
+
+func (a *Atom) Push(pair dict.Pair[style.Atom, AtomStyler]) *Atom {
+	a.lazyInit()
+
+	a.table.SetPairs(pair)
+	return a
+}
+
+func (a *Atom) Apply(text string, styles ...style.Atom) string {
+	a.lazyInit()
+
+	merged := style.MergeAtom(styles...)
+
+	for k, p := range a.table.All() {
+		if merged.HasAny(k) {
+			text = p(text)
+		}
+	}
+
+	return text
+}
---
+++ engine/render/styler/spec.go
@@ -0,0 +1,235 @@
+package styler
+
+import (
+	"github.com/Rafael24595/go-reacterm-core/engine/commons"
+	"github.com/Rafael24595/go-reacterm-core/engine/commons/structure/dict"
+	"github.com/Rafael24595/go-reacterm-core/engine/helper"
+	"github.com/Rafael24595/go-reacterm-core/engine/model/winsize"
+	"github.com/Rafael24595/go-reacterm-core/engine/render/style"
+)
+
+type SpecStyler func(style.Spec, winsize.Cols, string, winsize.Cols) (string, bool)
+
+func ps(k style.SpecKind, s SpecStyler) dict.Pair[style.SpecKind, SpecStyler] {
+	return dict.NewPair(k, s)
+}
+
+var DefaultSpecStyler = dict.NewInmutableLinkedMap(
+	ps(style.SpcKindFill, func(spec style.Spec, cols winsize.Cols, text string, logicalSize winsize.Cols) (string, bool) {
+		return fill(spec, cols, text, logicalSize), true
+	}),
+	ps(style.SpcKindTrimLeft, func(spec style.Spec, _ winsize.Cols, text string, logicalSize winsize.Cols) (string, bool) {
+		return trimLeft(spec, text, logicalSize), false
+	}),
+	ps(style.SpcKindTrimRight, func(spec style.Spec, _ winsize.Cols, text string, logicalSize winsize.Cols) (string, bool) {
+		return trimRight(spec, text, logicalSize), false
+	}),
+	ps(style.SpcKindPaddingCenter, func(spec style.Spec, cols winsize.Cols, text string, logicalSize winsize.Cols) (string, bool) {
+		return paddingCenter(spec, cols, text, logicalSize), false
+	}),
+	ps(style.SpcKindPaddingLeft, func(spec style.Spec, cols winsize.Cols, text string, logicalSize winsize.Cols) (string, bool) {
+		return paddingLeft(spec, cols, text, logicalSize), false
+	}),
+	ps(style.SpcKindPaddingRight, func(spec style.Spec, cols winsize.Cols, text string, logicalSize winsize.Cols) (string, bool) {
+		return paddingRight(spec, cols, text, logicalSize), false
+	}),
+	ps(style.SpcKindRepeatLeft, func(spec style.Spec, cols winsize.Cols, text string, logicalSize winsize.Cols) (string, bool) {
+		return repeatLeft(spec, cols, text, logicalSize), false
+	}),
+	ps(style.SpcKindRepeatRight, func(spec style.Spec, cols winsize.Cols, text string, logicalSize winsize.Cols) (string, bool) {
+		return repeatRight(spec, cols, text, logicalSize), false
+	}),
+)
+
+type Spec struct {
+	table *dict.LinkedMap[style.SpecKind, SpecStyler]
+}
+
+func NewSpec() *Spec {
+	instance := &Spec{}
+	return instance.lazyInit()
+}
+
+func NewDefault() *Spec {
+	return &Spec{
+		table: DefaultSpecStyler,
+	}
+}
+
+func (a *Spec) lazyInit() *Spec {
+	if a.table != nil {
+		return a
+	}
+
+	a.table = dict.NewLinkedMap[style.SpecKind, SpecStyler]()
+	return a
+}
+
+func (a *Spec) Push(pair ...dict.Pair[style.SpecKind, SpecStyler]) *Spec {
+	a.lazyInit()
+
+	a.table.SetPairs(pair...)
+	return a
+}
+
+func (a *Spec) Apply(spec style.Spec, size winsize.Winsize, text string, logicalSize winsize.Cols) string {
+	a.lazyInit()
+
+	exit := false
+	kind := spec.Kind()
+	for k, p := range a.table.All() {
+		if !kind.HasAny(k) {
+			continue
+		}
+
+		text, exit = p(spec, size.Cols, text, logicalSize)
+		if exit {
+			return text
+		}
+
+		logicalSize = style.SpecMeasureOf(k, spec, style.LayoutContext{
+			Cols:     size.Cols,
+			TextSize: logicalSize,
+		})
+	}
+
+	return text
+}
+
+func fill(styl style.Spec, cols winsize.Cols, data string, logicalSize winsize.Cols) string {
+	opts := helper.LogicalSizeOpts{
+		LogicalSize: logicalSize,
+	}
+
+	args := styl.Args()
+
+	size := commons.Mapd(args[style.KeyFillSize], cols)
+	size = min(cols, size)
+
+	return helper.FillRightWithOpts(data, size, opts)
+}
+
+func trimLeft(styl style.Spec, data string, logicalSize winsize.Cols) string {
+	if data == "" {
+		return data
+	}
+
+	args := styl.Args()
+
+	size := commons.Mapd[winsize.Cols](args[style.KeyTrimLeftSize], 0)
+	size = max(1, size)
+
+	elip := args[style.KeyTrimEllipsisText].Stringf()
+
+	opts := helper.TextTrimOpts{
+		LogicalSize:  logicalSize,
+		EllipsisText: elip,
+	}
+
+	return helper.TrimLeft(data, size, opts)
+}
+
+func trimRight(styl style.Spec, data string, logicalSize winsize.Cols) string {
+	if data == "" {
+		return data
+	}
+
+	args := styl.Args()
+
+	size := commons.Mapd[winsize.Cols](args[style.KeyTrimRightSize], 0)
+	size = max(1, size)
+
+	elip := args[style.KeyTrimEllipsisText].Stringf()
+
+	opts := helper.TextTrimOpts{
+		LogicalSize:  logicalSize,
+		EllipsisText: elip,
+	}
+
+	return helper.TrimRight(data, size, opts)
+}
+
+// TODO: Explore the risks of using cols as default
+func paddingCenter(styl style.Spec, cols winsize.Cols, data string, logicalSize winsize.Cols) string {
+	args := styl.Args()
+
+	size := commons.Mapd(args[style.KeyPaddingCenterSize], cols)
+	size = min(cols, size)
+
+	text := args[style.KeyPaddingCenterText].Stringf()
+
+	opts := helper.TextLayoutOpts{
+		LogicalSize: logicalSize,
+		Text:        text,
+	}
+
+	return helper.CenterWithOpts(data, size, opts)
+}
+
+func paddingLeft(styl style.Spec, cols winsize.Cols, data string, logicalSize winsize.Cols) string {
+	args := styl.Args()
+
+	size := commons.Mapd[winsize.Cols](args[style.KeyPaddingLeftSize], 0)
+	size = min(cols, size)
+
+	text := args[style.KeyPaddingLeftText].Stringf()
+
+	opts := helper.TextLayoutOpts{
+		LogicalSize: logicalSize,
+		Text:        text,
+	}
+
+	return helper.LeftWithOpts(data, size, opts)
+}
+
+func paddingRight(styl style.Spec, cols winsize.Cols, data string, logicalSize winsize.Cols) string {
+	args := styl.Args()
+
+	size := commons.Mapd[winsize.Cols](args[style.KeyPaddingRightSize], 0)
+	size = min(cols, size)
+
+	text := args[style.KeyPaddingRightText].Stringf()
+
+	opts := helper.TextLayoutOpts{
+		LogicalSize: logicalSize,
+		Text:        text,
+	}
+
+	return helper.RightWithOpts(data, size, opts)
+}
+
+func repeatLeft(styl style.Spec, cols winsize.Cols, data string, logicalSize winsize.Cols) string {
+	args := styl.Args()
+
+	size := commons.Mapd[winsize.Cols](args[style.KeyRepeatLeftSize], 0)
+	text := args[style.KeyRepeatLeftText].Stringf()
+
+	if text == "" {
+		text = data
+		data = ""
+	}
+
+	opts := helper.LogicalSizeOpts{
+		LogicalSize: logicalSize,
+	}
+
+	return helper.RepeatLeftWithOpts(data, text, min(cols, size), opts)
+}
+
+func repeatRight(styl style.Spec, cols winsize.Cols, data string, logicalSize winsize.Cols) string {
+	args := styl.Args()
+
+	size := commons.Mapd[winsize.Cols](args[style.KeyRepeatRightSize], 0)
+	text := args[style.KeyRepeatRightText].Stringf()
+
+	if text == "" {
+		text = data
+		data = ""
+	}
+
+	opts := helper.LogicalSizeOpts{
+		LogicalSize: logicalSize,
+	}
+
+	return helper.RepeatRightWithOpts(data, text, min(cols, size), opts)
+}
