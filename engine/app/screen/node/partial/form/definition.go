5c49230f2d611f9677316a2f770c3bfb62eb60af
Rafael Ruiz <rafaelmostoles24595@gmail.com>
2026-06-03 19:36:04 +0200

    feat: move form definitions to it's own file

---
+++ engine/app/screen/node/partial/form/definition.go
@@ -0,0 +1,24 @@
+package form
+
+import (
+	"github.com/Rafael24595/go-reacterm-core/engine/app/screen"
+	"github.com/Rafael24595/go-reacterm-core/engine/model/key"
+)
+
+var sources = screen.NewDefinition(
+	map[key.Action]key.Descriptor{
+		key.ActionEsc:       {Code: []string{"ESC"}, Detail: "Navigation Mode"},
+		key.ActionEnter:     {Code: []string{"RET"}, Detail: "Active selected"},
+		key.ActionArrowUp:   {Code: []string{"↑"}, Detail: "Move first"},
+		key.ActionArrowDown: {Code: []string{"↓"}, Detail: "Move last"},
+	},
+	[]key.Action{
+		key.ActionEsc,
+		key.ActionEnter,
+		key.ActionArrowLeft,
+		key.ActionArrowRight,
+		key.ActionArrowUp,
+		key.ActionArrowDown,
+		key.CustomActionPointer,
+	},
+)
--- engine/app/screen/node/partial/form/form.go
+++ engine/app/screen/node/partial/form/form.go
@@ -20,24 +20,6 @@ import (
 
 const Name = "form"
 
-var sources = screen.NewDefinition(
-	map[key.Action]key.Descriptor{
-		key.ActionEsc:       {Code: []string{"ESC"}, Detail: "Navigation Mode"},
-		key.ActionEnter:     {Code: []string{"RET"}, Detail: "Active selected"},
-		key.ActionArrowUp:   {Code: []string{"↑"}, Detail: "Move first"},
-		key.ActionArrowDown: {Code: []string{"↓"}, Detail: "Move last"},
-	},
-	[]key.Action{
-		key.ActionEsc,
-		key.ActionEnter,
-		key.ActionArrowLeft,
-		key.ActionArrowRight,
-		key.ActionArrowUp,
-		key.ActionArrowDown,
-		key.CustomActionPointer,
-	},
-)
-
 type Form struct {
 	reference string
 	pointer   uint8
