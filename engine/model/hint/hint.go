9cfab6abfca74a46fb806808d241fdcb586a81e1
Rafael Ruiz <rafaelmostoles24595@gmail.com>
2026-06-11 00:25:22 +0200

    feat: improve hint codebase: - add max provider - integrate fill with provider - add percent constructor

--- engine/model/hint/hint.go
+++ engine/model/hint/hint.go
@@ -1,27 +1,41 @@
 package hint
 
-import "github.com/Rafael24595/go-reacterm-core/engine/helper/math"
+import (
+	"github.com/Rafael24595/go-reacterm-core/engine/helper/math"
+)
+
+type provider[T math.Number] func(max T) T
 
 type Size[T math.Number] struct {
-	size T
-	fill bool
+	provider provider[T]
 }
 
 func Fixed[T math.Number](size T) Size[T] {
 	return Size[T]{
-		size: size,
+		provider: func(T) T {
+			return size
+		},
+	}
+}
+
+func Percent[T math.Number](size T) Size[T] {
+	return Size[T]{
+		provider: func(max T) T {
+			return (size * max) / 100
+		},
 	}
 }
 
 func Maximize[T math.Number]() Size[T] {
 	return Size[T]{
-		fill: true,
+		provider: func(max T) T {
+			return max
+		},
 	}
 }
 
-func (h Size[T]) Min(maxSize T) T {
-	if h.fill {
-		return maxSize
-	}
-	return min(h.size, maxSize)
+func (h Size[T]) Min(max T) T {
+	return min(
+		h.provider(max), max,
+	)
 }
