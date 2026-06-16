4ff0687cacd5517aa76f4bfe6c6af595a1c98e88
Rafael Ruiz <rafaelmostoles24595@gmail.com>
2026-06-16 19:15:37 +0200

    feat: add use helper to tick

--- engine/app/screen/behavior/tick/tick.go
+++ engine/app/screen/behavior/tick/tick.go
@@ -1,11 +1,17 @@
 package tick
 
 import (
+	"slices"
+
 	"github.com/Rafael24595/go-reacterm-core/engine/app/screen"
 	"github.com/Rafael24595/go-reacterm-core/engine/app/screen/behavior"
+	"github.com/Rafael24595/go-reacterm-core/engine/app/state"
+	"github.com/Rafael24595/go-reacterm-core/engine/model/key"
 )
 
 const Tag = "behavior:tick"
+
+type Middleware func(uiState *state.UIState, event screen.Event, context behavior.Context[screen.TickFunc]) screen.Result
 
 func Apply(node screen.Node, decorator behavior.Tick) screen.Node {
 	return behavior.Apply(
@@ -21,7 +27,37 @@ func Wrap(decorator behavior.Tick) behavior.Behavior {
 		)
 
 		node.Tags.Add(Tag)
-		
+
 		return node
+	}
+}
+
+func Use(node screen.Node, middleware Middleware) screen.Node {
+	return Apply(node, use(middleware))
+}
+
+func use(middleware Middleware) behavior.Tick {
+	return func(target behavior.Target, next screen.TickFunc) screen.TickFunc {
+		context := behavior.NewContext(target, next)
+		return func(uiState *state.UIState, event screen.Event) screen.Result {
+			return middleware(uiState, event, context)
+		}
+	}
+}
+
+func OnKey(node screen.Node, middleware Middleware, keys ...key.Action) screen.Node {
+	return Apply(node, onKey(keys, middleware))
+}
+
+func onKey(keys []key.Action, middleware Middleware) behavior.Tick {
+	return func(target behavior.Target, next screen.TickFunc) screen.TickFunc {
+		context := behavior.NewContext(target, next)
+
+		return func(uiState *state.UIState, event screen.Event) screen.Result {
+			if slices.Contains(keys, event.Key.Code) {
+				return middleware(uiState, event, context)
+			}
+			return next(uiState, event)
+		}
 	}
 }
--- engine/app/screen/behavior/tick/tick_test.go
+++ engine/app/screen/behavior/tick/tick_test.go
@@ -9,6 +9,7 @@ import (
 	"github.com/Rafael24595/go-reacterm-core/engine/app/screen/behavior"
 	"github.com/Rafael24595/go-reacterm-core/engine/app/state"
 	"github.com/Rafael24595/go-reacterm-core/engine/commons/structure/set"
+	"github.com/Rafael24595/go-reacterm-core/engine/model/key"
 
 	screen_test "github.com/Rafael24595/go-reacterm-core/test/engine/app/screen"
 )
@@ -90,4 +91,111 @@ func TestWrap_TargetIsCorrect(t *testing.T) {
 	wrapped.Screen.Tick(&state.UIState{}, screen.Event{})
 
 	assert.Equal(t, mock.Name, captured.Name)
+}
+
+func TestUse_ExecutesMiddleware_AndPassesContext(t *testing.T) {
+	mwCalled := uint(0)
+	nxCalled := uint(0)
+
+	name := "test-node"
+
+	middleware := func(uiState *state.UIState, event screen.Event, ctx behavior.Context[screen.TickFunc]) screen.Result {
+		mwCalled += 1
+		assert.Equal(t, name, ctx.Target.Name)
+		return ctx.Next(uiState, event)
+	}
+
+	mock := screen_test.MockNode{
+		Name: name,
+		Tick: func(uiState *state.UIState, event screen.Event) screen.Result {
+			nxCalled += 1
+			return screen.EmptyResult()
+		},
+	}
+
+	node := Use(mock.ToNode(), middleware)
+	node.Screen.Tick(&state.UIState{}, screen.Event{})
+
+	assert.Equal(t, 1, mwCalled)
+	assert.Equal(t, 1, nxCalled)
+}
+
+func TestUse_CanShortCircuitChain(t *testing.T) {
+	mwCalled := uint(0)
+	nxCalled := uint(0)
+
+	middleware := func(uiState *state.UIState, event screen.Event, ctx behavior.Context[screen.TickFunc]) screen.Result {
+		mwCalled += 1
+		return screen.EmptyResult()
+	}
+
+	mock := screen_test.MockNode{
+		Name: "test-node",
+		Tick: func(uiState *state.UIState, event screen.Event) screen.Result {
+			nxCalled += 1
+			return screen.EmptyResult()
+		},
+	}
+
+	node := Use(mock.ToNode(), middleware)
+	node.Screen.Tick(&state.UIState{}, screen.Event{})
+
+	assert.Equal(t, 1, mwCalled)
+	assert.Equal(t, 0, nxCalled)
+}
+
+func TestOnKey_ExecutesMiddleware_WhenKeyMatches(t *testing.T) {
+	called := uint(0)
+
+	middleware := func(uiState *state.UIState, event screen.Event, ctx behavior.Context[screen.TickFunc]) screen.Result {
+		called += 1
+		return screen.EmptyResult()
+	}
+
+	mock := screen_test.MockNode{
+		Name: "test-node",
+	}
+
+	node := OnKey(mock.ToNode(), middleware, key.ActionEnter, key.ActionEsc)
+
+	matchingEvent := screen.Event{
+		Key: key.Key{
+			Code: key.ActionEnter,
+		},
+	}
+
+	node.Screen.Tick(&state.UIState{}, matchingEvent)
+
+	assert.Equal(t, 1, called)
+}
+
+func TestOnKey_BypassesMiddleware_AndCallsNext_WhenKeyDoesNotMatch(t *testing.T) {
+	mwCalled := uint(0)
+	nxCalled := uint(0)
+
+	middleware := func(uiState *state.UIState, event screen.Event, ctx behavior.Context[screen.TickFunc]) screen.Result {
+		mwCalled += 1
+		return screen.EmptyResult()
+	}
+
+	mock := screen_test.MockNode{
+		Name: "test-node",
+		Tick: func(uiState *state.UIState, event screen.Event) screen.Result {
+			nxCalled += 1
+			return screen.EmptyResult()
+		},
+	}
+
+	node := OnKey(mock.ToNode(), middleware, key.ActionEnter, key.ActionEsc)
+
+	matchingEvent := screen.Event{
+		Key: key.Key{
+			Code: key.ActionDelete,
+		},
+	}
+
+	node.Screen.Tick(&state.UIState{}, matchingEvent)
+
+	assert.Equal(t, 0, mwCalled)
+	assert.Equal(t, 1, nxCalled)
 }
