4f4b72b057d201cef4ce11810783f795381f4c4c
Rafael Ruiz <rafaelmostoles24595@gmail.com>
2026-06-19 01:02:38 +0200

    feat: add clip demo

--- wrapper/screen/test_select.go
+++ wrapper/screen/test_select.go
@@ -30,6 +30,7 @@ func NewTestSelect() screen.Node {
 		input.NewMenuOption("opt_chk", *text.NewFragment("[Prim] Option Check"), NewTestCheck),
 		input.NewMenuOption("opt_txi", *text.NewFragment("[Prim] Option TextInput"), NewTestTextInput),
 		input.NewMenuOption("opt_tlk", *text.NewFragment("[Prim] Option Talk"), NewTestTalk),
+		input.NewMenuOption("opt_clp", *text.NewFragment("[Prim] Option Clip"), NewTestClip),
 		input.NewMenuOption("opt_frm", *text.NewFragment("[Comp] Option Form"), NewTestForm),
 		input.NewMenuOption("opt_hsk", *text.NewFragment("[Demo] Option HStack"), NewTestHStack),
 	)
