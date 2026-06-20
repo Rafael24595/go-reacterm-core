f40a02f88198d38029061d568b32465edea1fd7a
Rafael Ruiz <rafaelmostoles24595@gmail.com>
2026-06-20 11:46:18 +0200

    feat: add screen constants

---
+++ engine/app/screen/const.go
@@ -0,0 +1,6 @@
+package screen
+
+const (
+	MessageModified   = "the node should not be modified after initialization"
+	MessageNewElement = "no new elements should be added after initialization"
+)
