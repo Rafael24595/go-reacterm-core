fc0c8dba5f8c93cfbb36a92c71b986c1a69b4f59
Rafael Ruiz <rafaelmostoles24595@gmail.com>
2026-06-29 19:26:46 +0200

    feat: add stack structure

---
+++ engine/commons/structure/stack/stack.go
@@ -0,0 +1,70 @@
+package stack
+
+type Stack[T any] struct {
+	data []T
+}
+
+func New[T any](limit uint) *Stack[T] {
+	return &Stack[T]{
+		data: make([]T, 0, limit),
+	}
+}
+
+func (s *Stack[T]) Len() uint {
+	return uint(len(s.data))
+}
+
+func (s *Stack[T]) Cap() uint {
+	return uint(cap(s.data))
+}
+
+func (s *Stack[T]) Peek() (T, bool) {
+	var zero T
+
+	if len(s.data) == 0 {
+		return zero, false
+	}
+
+	return s.data[len(s.data)-1], true
+}
+
+func (s *Stack[T]) Push(item T) (T, bool) {
+	if len(s.data) < cap(s.data) {
+		var zero T
+		s.data = append(s.data, item)
+		return zero, false
+	}
+
+	discarded := s.data[0]
+
+	copy(s.data, s.data[1:])
+	s.data[len(s.data)-1] = item
+
+	return discarded, true
+}
+
+func (s *Stack[T]) Pop() (T, bool) {
+	var zero T
+	if len(s.data) == 0 {
+		return zero, false
+	}
+
+	last := len(s.data) - 1
+	node := s.data[last]
+
+	s.data[last] = zero
+
+	s.data = s.data[:last]
+
+	return node, true
+}
+
+func (s *Stack[T]) Clear() {
+	var zero T
+
+	for i := range s.data {
+		s.data[i] = zero
+	}
+
+	s.data = s.data[:0]
+}
---
+++ engine/commons/structure/stack/stack_test.go
@@ -0,0 +1,92 @@
+package stack
+
+import (
+	"testing"
+
+	assert "github.com/Rafael24595/go-assert/assert/test"
+)
+
+func TestStack_PushPop_LIFO(t *testing.T) {
+	s := New[int](3)
+
+	s.Push(1)
+	s.Push(2)
+	s.Push(3)
+
+	v, ok := s.Pop()
+	assert.True(t, ok)
+	assert.Equal(t, 3, v)
+
+	v, ok = s.Pop()
+	assert.True(t, ok)
+	assert.Equal(t, 2, v)
+
+	v, ok = s.Pop()
+	assert.True(t, ok)
+	assert.Equal(t, 1, v)
+
+	_, ok = s.Pop()
+	assert.False(t, ok)
+}
+
+func TestStack_ReplaceWhenFull(t *testing.T) {
+	s := New[int](3)
+
+	_, r1 := s.Push(1)
+	_, r2 := s.Push(2)
+	_, r3 := s.Push(3)
+
+	assert.False(t, r1 || r2 || r3)
+
+	discarded, replaced := s.Push(4)
+	assert.True(t, replaced)
+	assert.Equal(t, 1, discarded)
+
+	v, _ := s.Pop()
+	assert.Equal(t, 4, v)
+
+	v, _ = s.Pop()
+	assert.Equal(t, 3, v)
+
+	v, _ = s.Pop()
+	assert.Equal(t, 2, v)
+}
+
+func TestStack_Clear(t *testing.T) {
+	s := New[int](3)
+
+	s.Push(1)
+	s.Push(2)
+
+	s.Clear()
+	assert.Equal(t, 0, s.Len())
+
+	_, ok := s.Pop()
+	assert.False(t, ok)
+
+	s.Push(10)
+
+	v, ok := s.Pop()
+	assert.True(t, ok)
+	assert.Equal(t, 10, v)
+}
+
+func TestStack_Len(t *testing.T) {
+	s := New[int](3)
+
+	assert.Equal(t, 0, s.Len())
+
+	s.Push(1)
+
+	assert.Equal(t, 1, s.Len())
+}
+
+func TestStack_Capacity1(t *testing.T) {
+	s := New[int](3)
+
+	assert.Equal(t, 3, s.Cap())
+
+	s = New[int](6)
+
+	assert.Equal(t, 6, s.Cap())
+}
