9e02c51bdfe3b2258870e645b1d6ae619f6bcc23
Rafael Ruiz <rafaelmostoles24595@gmail.com>
2026-07-05 11:32:08 +0200

    feat: add `wordFrag` to wrap package

---
+++ engine/render/wrap/frag.go
@@ -0,0 +1,64 @@
+package wrap
+
+import (
+	"github.com/Rafael24595/go-reacterm-core/engine/model/winsize"
+	"github.com/Rafael24595/go-reacterm-core/engine/render/text"
+)
+
+type measureResolver func(winsize.Cols, ...wordFrag) winsize.Cols
+
+type wordFrag struct {
+	Base     *text.Fragment
+	measured bool
+	cols     winsize.Cols
+	measure  winsize.Cols
+}
+
+func newWordFrag(frag *text.Fragment) *wordFrag {
+	return &wordFrag{
+		Base:     frag,
+		measured: false,
+		cols:     0,
+		measure:  0,
+	}
+}
+
+func (w *wordFrag) Measure(cols winsize.Cols) winsize.Cols {
+	return w.measureWith(cols, fragmentMeasure)
+}
+
+func (w *wordFrag) measureWith(
+	cols winsize.Cols,
+	resolver measureResolver,
+) winsize.Cols {
+	if !w.measured || w.cols != cols {
+		w.measure = resolver(cols, *w)
+		w.cols = cols
+		w.measured = true
+	}
+
+	return w.measure
+}
+
+func toWordFrag(frags ...text.Fragment) []wordFrag {
+	result := make([]wordFrag, len(frags))
+	for i, f := range frags {
+		result[i] = *newWordFrag(&f)
+	}
+	return result
+}
+
+func appendFragments(dst []text.Fragment, src ...wordFrag) []text.Fragment {
+    for _, f := range src {
+        dst = append(dst, *f.Base)
+    }
+    return dst
+}
+
+func fragmentMeasure(cols winsize.Cols, frags ...wordFrag) winsize.Cols {
+	measure := winsize.Cols(0)
+	for _, f := range frags {
+		measure += text.FragmentMeasure(cols, *f.Base)
+	}
+	return measure
+}
---
+++ engine/render/wrap/frag_test.go
@@ -0,0 +1,89 @@
+package wrap
+
+import (
+	"testing"
+
+	assert "github.com/Rafael24595/go-assert/assert/test"
+	"github.com/Rafael24595/go-reacterm-core/engine/model/winsize"
+	"github.com/Rafael24595/go-reacterm-core/engine/render/text"
+)
+
+func TestWordFragMeasure_CacheSameCols(t *testing.T) {
+	w := newWordFrag(
+		text.NewFragment("golang"),
+	)
+
+	calls := uint(0)
+
+	resolver := func(cols winsize.Cols, frags ...wordFrag) winsize.Cols {
+		calls++
+		return 42
+	}
+
+	first := w.measureWith(80, resolver)
+	second := w.measureWith(80, resolver)
+
+	assert.Equal(t, first, second)
+	assert.Equal(t, winsize.Cols(80), w.cols)
+
+	assert.Equal(t, 1, calls)
+}
+
+func TestWordFragMeasure_RecalculateOnColsChange(t *testing.T) {
+	w := newWordFrag(
+		text.NewFragment("golang"),
+	)
+
+	calls := uint(0)
+
+	resolver := func(cols winsize.Cols, frags ...wordFrag) winsize.Cols {
+		calls++
+		return 42
+	}
+
+	_ = w.measureWith(80, resolver)
+	m40 := w.measureWith(40, resolver)
+
+	assert.Equal(t, winsize.Cols(40), w.cols)
+	assert.Equal(t, m40, w.measure)
+
+	assert.Equal(t, 2, calls)
+}
+
+func TestWordFragMeasure_CacheAfterColsChange(t *testing.T) {
+	w := newWordFrag(
+		text.NewFragment("golang"),
+	)
+
+	calls := uint(0)
+
+	resolver := func(cols winsize.Cols, frags ...wordFrag) winsize.Cols {
+		calls++
+		return 42
+	}
+
+	w.measureWith(80, resolver)
+	w.measureWith(40, resolver)
+	w.measureWith(40, resolver)
+
+	assert.Equal(t, uint(2), calls)
+}
+
+func TestWordFragMeasure_RecalculateWhenReturningToPreviousCols(t *testing.T) {
+	w := newWordFrag(
+		text.NewFragment("golang"),
+	)
+
+	calls := uint(0)
+
+	resolver := func(cols winsize.Cols, frags ...wordFrag) winsize.Cols {
+		calls++
+		return 42
+	}
+
+	w.measureWith(80, resolver)
+	w.measureWith(40, resolver)
+	w.measureWith(80, resolver)
+
+	assert.Equal(t, uint(3), calls)
+}
