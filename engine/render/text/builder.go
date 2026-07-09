1e3690fd0360040dfca733e1bdbe8d967e5b0f61
Rafael Ruiz <rafaelmostoles24595@gmail.com>
2026-07-10 00:57:48 +0200

    feat: add line builder

--- engine/render/text/builder.go
+++ engine/render/text/builder.go
@@ -55,3 +55,40 @@ func (f *BuilderFrag) Frag() Frag {
 		Spec: f.Spec,
 	}
 }
+
+type BuilderLine struct {
+	Order uint16
+	Text  []Frag
+	Spec  spec.Spec
+}
+
+func (f *BuilderLine) SetOrder(order uint16) *BuilderLine {
+	f.Order = order
+	return f
+}
+
+func (f *BuilderLine) AddFrags(text ...Frag) *BuilderLine {
+	f.Text = append(f.Text, text...)
+	return f
+}
+
+func (f *BuilderLine) AddBuilder(builder ...*BuilderFrag) *BuilderLine {
+	for _, b := range builder {
+		f.Text = append(f.Text, b.Frag())
+	}
+	return f
+}
+
+func (f *BuilderLine) AddSpec(styles ...spec.Spec) *BuilderLine {
+	newSpec := spec.Merge(styles...)
+	f.Spec = spec.Merge(f.Spec, newSpec)
+	return f
+}
+
+func (f *BuilderLine) Frag() Line {
+	return Line{
+		Order: f.Order,
+		Text:  f.Text,
+		Spec:  f.Spec,
+	}
+}
