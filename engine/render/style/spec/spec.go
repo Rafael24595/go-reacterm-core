44f76bae6f892ed350e2f5593c4180d8ec4bf7f3
Rafael Ruiz <rafaelmostoles24595@gmail.com>
2026-07-13 01:33:28 +0200

    refactor: use spec builder on `Merge` and `Erase`

--- engine/render/style/spec/spec.go
+++ engine/render/style/spec/spec.go
@@ -16,55 +16,23 @@ func Empty() Spec {
 	return New(KindNone, args{})
 }
 
-func fromKind(kind Kind) Spec {
-	return New(kind, args{})
-}
-
-func Merge(styles ...Spec) Spec {
-	kind := KindNone
-	args := args{}
-
-	for _, style := range styles {
-		kind |= style.kind
-		args.Copy(style.args)
-	}
-
-	return New(kind, args)
-}
-
-func Erase(target Spec, styles Kind) (Spec, Spec) {
-	removedKind := target.kind & styles
-
-	removedSpec := fromKind(removedKind)
-	if removedKind == KindNone {
-		return target, removedSpec
-	}
-
-	for _, desc := range kindRegistry {
-		if removedKind&desc.Kind == 0 {
-			continue
-		}
-
-		for _, key := range desc.Args {
-			val, ok := target.args.TryGet(key)
-			if !ok {
-				continue
-			}
-
-			removedSpec.args.Set(key, val)
-			target.args.Delete(key)
-		}
-	}
-
-	target.kind &= ^styles
-
-	return target, removedSpec
-}
-
 func (s Spec) Kind() Kind {
 	return s.kind
 }
 
 func (s Spec) Args() argMap {
 	return s.args.Items()
+}
+
+func Merge(styles ...Spec) Spec {
+	return newBuilder().
+		apply(styles...).
+		build()
+}
+
+func Erase(spec Spec, kinds Kind) (Spec, Spec) {
+	builder := newBuilder(spec)
+	deleted := builder.erase(kinds)
+
+	return builder.build(), deleted
 }
