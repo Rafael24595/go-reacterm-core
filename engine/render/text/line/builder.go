ab9904ef841a8ceb56c83ffd20cec1a14cf2f3dd
Rafael Ruiz <rafaelmostoles24595@gmail.com>
2026-07-20 00:58:50 +0200

    refactor: standardize method receiver names

--- engine/render/text/line/builder.go
+++ engine/render/text/line/builder.go
@@ -7,37 +7,37 @@ import (
 
 type Builder struct {
 	Order uint16
-	Text  []frag.Frag
 	Spec  spec.Spec
+	Text  []frag.Frag
 }
 
-func (f *Builder) SetOrder(order uint16) *Builder {
-	f.Order = order
-	return f
+func (b *Builder) SetOrder(order uint16) *Builder {
+	b.Order = order
+	return b
 }
 
-func (f *Builder) AddFrags(text ...frag.Frag) *Builder {
-	f.Text = append(f.Text, text...)
-	return f
-}
-
-func (f *Builder) AddBuilder(builder ...*frag.Builder) *Builder {
-	for _, b := range builder {
-		f.Text = append(f.Text, b.Frag())
-	}
-	return f
-}
-
-func (f *Builder) AddSpec(styles ...spec.Spec) *Builder {
+func (b *Builder) AddSpec(styles ...spec.Spec) *Builder {
 	newSpec := spec.Merge(styles...)
-	f.Spec = spec.Merge(f.Spec, newSpec)
-	return f
+	b.Spec = spec.Merge(b.Spec, newSpec)
+	return b
 }
 
-func (f *Builder) Line() Line {
+func (b *Builder) AddFrags(frags ...frag.Frag) *Builder {
+	b.Text = append(frags, b.Text...)
+	return b
+}
+
+func (b *Builder) AddBuilder(builder ...*frag.Builder) *Builder {
+	for _, f := range builder {
+		b.Text = append(b.Text, f.Frag())
+	}
+	return b
+}
+
+func (b *Builder) Line() Line {
 	return Line{
-		Order: f.Order,
-		Text:  f.Text,
-		Spec:  f.Spec,
+		Order: b.Order,
+		Spec:  b.Spec,
+		Text:  b.Text,
 	}
 }
