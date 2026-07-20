553814497574af8b0b0b19a1ead965395d4d3764
Rafael Ruiz <rafaelmostoles24595@gmail.com>
2026-07-21 01:04:17 +0200

    feat: improve `Line` constructors: - add `empty` helper - add `OrderedText`, `FromSpec` and `FromString`

--- engine/render/text/line/lines.go
+++ engine/render/text/line/lines.go
@@ -5,25 +5,67 @@ import (
 	"github.com/Rafael24595/go-reacterm-core/engine/render/text/frag"
 )
 
-func Empty(size ...int) *Line {
+func empty(
+	order uint16,
+	spec spec.Spec,
+	size ...int,
+) *Line {
 	bufferSize := 0
 	if len(size) > 0 {
 		bufferSize = size[0]
 	}
 
-	return FromFrags(
-		make([]frag.Frag, 0, bufferSize)...,
+	return newLine(
+		order,
+		spec,
+		make([]frag.Frag, 0, bufferSize),
+	)
+}
+
+func Empty(size ...int) *Line {
+	return empty(
+		0,
+		spec.Empty(),
+		size...,
+	)
+}
+
+func OrderedText(order uint16, text ...string) Line {
+	return *newLine(
+		order,
+		spec.Empty(),
+		frag.FromStrings(text...),
+	)
+}
+
+func FromSpec(spec spec.Spec) Line {
+	return *newLine(
+		0,
+		spec,
+		make([]frag.Frag, 0),
 	)
 }
 
 func FromMeta(other *Line, size ...int) *Line {
-	return Empty(size...).
-		CopyMeta(other)
+	return empty(
+		other.Order,
+		other.Spec,
+		size...,
+	)
+}
+
+func FromString(text ...string) Line {
+	return *newLine(
+		0,
+		spec.Empty(),
+		frag.FromStrings(text...),
+	)
 }
 
 func FromFrags(frags ...frag.Frag) *Line {
-	return &Line{
-		Text: frags,
-		Spec: spec.Empty(),
-	}
+	return newLine(
+		0,
+		spec.Empty(),
+		frags,
+	)
 }
