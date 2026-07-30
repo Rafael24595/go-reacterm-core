0fbdc58e43d6e7324393772b5489c96c526422c7
Rafael Ruiz <rafaelmostoles24595@gmail.com>
2026-07-31 00:47:35 +0200

    refactor: move tests helpers to it's own file

--- engine/render/wrap/helper_test.go
+++ engine/render/wrap/helper_test.go
@@ -2,7 +2,58 @@ package wrap
 
 import (
 	"strings"
+	"testing"
+
+	assert "github.com/Rafael24595/go-assert/assert/test"
+	
+	"github.com/Rafael24595/go-reacterm-core/engine/render/text/frag"
+	"github.com/Rafael24595/go-reacterm-core/engine/render/text/line"
+
+	text_test "github.com/Rafael24595/go-reacterm-core/test/engine/render/text"
 )
+
+func assembleLines(t *testing.T, lines ...line.Line) string {
+	t.Helper()
+
+	var sb strings.Builder
+
+	for i, l := range lines {
+		_, err := sb.WriteString(
+			text_test.LineToString(l),
+		)
+
+		assert.Nil(t, err)
+
+		if i < len(lines)-1 {
+			_, err := sb.WriteString("\n")
+			assert.Nil(t, err)
+		}
+	}
+
+	return sb.String()
+}
+
+func benchmarkText(size int) string {
+	const sample = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. "
+
+	var b strings.Builder
+	b.Grow(size)
+
+	for b.Len() < size {
+		b.WriteString(sample)
+	}
+
+	return b.String()[:size]
+}
+
+func benchmarkLine(size int) line.Line {
+	return line.FromFrags(
+		frag.FromStrings(
+			benchmarkText(size),
+		)...,
+	)
+}
+
 
 func wordsToString(words []word, frags []wordFrag) string {
 	var sb strings.Builder
--- engine/render/wrap/wrap_test.go
+++ engine/render/wrap/wrap_test.go
@@ -16,37 +16,6 @@ import (
 	text_test "github.com/Rafael24595/go-reacterm-core/test/engine/render/text"
 )
 
-func assembleLines(t *testing.T, lines ...line.Line) string {
-	t.Helper()
-
-	var sb strings.Builder
-
-	for i, l := range lines {
-		_, err := sb.WriteString(
-			text_test.LineToString(l),
-		)
-
-		assert.Nil(t, err)
-
-		if i < len(lines)-1 {
-			_, err := sb.WriteString("\n")
-			assert.Nil(t, err)
-		}
-	}
-
-	return sb.String()
-}
-
-func benchmarkLine(words int) line.Line {
-	builder := strings.Builder{}
-
-	for range words {
-		builder.WriteString("Lorem ")
-	}
-
-	return line.FromString(builder.String())
-}
-
 func TestWrapOnce(t *testing.T) {
 	tests := []struct {
 		name         string
