2f2a63afc8d9b052d29f03b3fb811b907cd59916
Rafael Ruiz <rafaelmostoles24595@gmail.com>
2026-07-30 02:15:46 +0200

    refactor: move test helpers to it's own file

--- engine/render/wrap/word_test.go
+++ engine/render/wrap/word_test.go
@@ -11,36 +11,6 @@ import (
 	"github.com/Rafael24595/go-reacterm-core/engine/render/text/line"
 )
 
-func wordsToString(words []word, frags []wordFrag) string {
-	var sb strings.Builder
-	for _, words := range words {
-		sb.WriteString(
-			wordToString(words, frags),
-		)
-	}
-	return sb.String()
-}
-
-func wordToString(word word, frags []wordFrag) string {
-	return fragsToString(frags[word.start:word.end])
-}
-
-func wordsToStrings(tokens []word, frags []wordFrag) []string {
-	out := make([]string, len(tokens))
-	for i, word := range tokens {
-		out[i] = wordToString(word, frags)
-	}
-	return out
-}
-
-func fragsToString(frags []wordFrag) string {
-	var b strings.Builder
-	for _, f := range frags {
-		b.WriteString(f.Base.Text())
-	}
-	return b.String()
-}
-
 func TestSplitLineWords(t *testing.T) {
 	tests := []struct {
 		name     string
