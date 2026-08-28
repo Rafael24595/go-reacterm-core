2395168ce1711ef45cad9b72a04a3fa61f001a4f
Rafael Ruiz <rafaelmostoles24595@gmail.com>
2026-08-28 20:03:06 +0200

    refactor: improve `Runtime` sources: - update nomenclature - update session ID generator - use methods with references

--- engine/app/runtime/runtime.go
+++ engine/app/runtime/runtime.go
@@ -2,33 +2,33 @@ package runtime
 
 import (
 	"fmt"
-	"math/rand"
+	"math/rand/v2"
 	"time"
 )
 
 var Instance *Runtime
 
 type Runtime struct {
-	sessionId string
+	sessionID string
 	timestamp int64
 }
 
 func init() {
-	rnd := rand.New(rand.NewSource(time.Now().UnixNano()))
 	Instance = &Runtime{
-		sessionId: newSessionId(rnd),
+		sessionID: newSessionId(),
 		timestamp: time.Now().UnixMilli(),
 	}
 }
 
-func newSessionId(rnd *rand.Rand) string {
-	return fmt.Sprintf("%d-%04x", time.Now().UnixNano(), rnd.Uint32())
+func newSessionId() string {
+	now := time.Now().UnixNano()
+	return fmt.Sprintf("%d-%04x", now, rand.Uint32())
 }
 
-func (r Runtime) SessionId() string {
-	return r.sessionId
+func (r *Runtime) SessionID() string {
+	return r.sessionID
 }
 
-func (r Runtime) Timestamp() int64 {
+func (r *Runtime) Timestamp() int64 {
 	return r.timestamp
 }
