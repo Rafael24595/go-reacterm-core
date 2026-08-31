07c46ea957a087588e31614a8efb80524b6908b7
Rafael Ruiz <rafaelmostoles24595@gmail.com>
2026-09-01 00:19:53 +0200

    docs: add `UIState` sources documentation

--- engine/app/state/state.go
+++ engine/app/state/state.go
@@ -2,12 +2,14 @@ package state
 
 import "github.com/Rafael24595/go-reacterm-core/engine/app/store"
 
+// UIState aggregates all sub-contexts representing the visual and operational state of the UI.
 type UIState struct {
 	Helper HelperContext
 	Pager  PagerContext
 	Store  *store.Store
 }
 
+// NewUIState initializes a UIState with default contexts and a fresh Store instance.
 func NewUIState() *UIState {
 	return &UIState{
 		Helper: HelperContext{},
