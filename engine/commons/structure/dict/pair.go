23198d332a8191570f6d06a8cee6c3cb825df4e7
Rafael Ruiz <rafaelmostoles24595@gmail.com>
2026-09-06 01:25:31 +0200

    docs: add `Pair` sources documentation

--- engine/commons/structure/dict/pair.go
+++ engine/commons/structure/dict/pair.go
@@ -1,10 +1,14 @@
 package dict
 
+// Pair represents a key-value pair.
 type Pair[K any, V any] struct {
+	// Key is the key of the pair.
 	Key   K
+	// Value is the value associated with the key.
 	Value V
 }
 
+// NewPair creates a new Pair with the given key and value.
 func NewPair[K any, V any](key K, value V) Pair[K, V] {
 	return Pair[K, V]{
 		Key:   key,
@@ -12,6 +16,7 @@ func NewPair[K any, V any](key K, value V) Pair[K, V] {
 	}
 }
 
+// P is a shorthand function to create a new Pair.
 func P[K any, V any](key K, value V) Pair[K, V] {
 	return NewPair(key, value)
 }
