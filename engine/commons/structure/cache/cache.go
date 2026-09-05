4ce446d6d05f049e9721e33bcb36b2f14e005683
Rafael Ruiz <rafaelmostoles24595@gmail.com>
2026-09-06 01:24:26 +0200

    docs: add `Cache` sources documentation

--- engine/commons/structure/cache/cache.go
+++ engine/commons/structure/cache/cache.go
@@ -1,9 +1,15 @@
 package cache
 
+// Cache represents a generic cache structure with basic operations.
 type Cache[T, K any] struct {
+	// Get retrieves the value associated with the given key.
 	Get func(key T) (K, bool)
+	// Put adds or updates the value associated with the given key.
 	Put func(key T, val K)
+	// Del removes the value associated with the given key.
 	Del func(key T)
+	// Len returns the number of items in the cache.
 	Len func() uint
+	// Cls clears all items from the cache.
 	Cls func()
 }
