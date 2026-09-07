d01e6d3cbf24a6edc5c5c672e12740160bbfae26
Rafael Ruiz <rafaelmostoles24595@gmail.com>
2026-09-07 20:03:23 +0200

    docs: add `Item` sources documentation

--- engine/commons/structure/list/item.go
+++ engine/commons/structure/list/item.go
@@ -1,9 +1,14 @@
 package list
 
+// Item represents a node in a doubly linked list.
 type Item[T any] struct {
+	// next points to the next item in the list. It is nil if the item is not part of any list.
 	next *Item[T]
+	// prev points to the previous item in the list. It is nil if the item is not part of any list.
 	prev *Item[T]
+	// list points to the list that contains this item. It is nil if the item is not part of any list.
 	list *List[T]
+	// Data holds the value of the list item.
 	Data T
 }
 
@@ -13,6 +18,7 @@ func newItem[T any](data T) *Item[T] {
 	}
 }
 
+// Next returns the next list item and true, or (nil, false) if e is the last item.
 func (e *Item[T]) Next() (*Item[T], bool) {
 	if e.list == nil {
 		return nil, false
@@ -26,6 +32,7 @@ func (e *Item[T]) Next() (*Item[T], bool) {
 	return next, true
 }
 
+// Prev returns the previous list item and true, or (nil, false) if e is the first item.
 func (e *Item[T]) Prev() (*Item[T], bool) {
 	if e.list == nil {
 		return nil, false
