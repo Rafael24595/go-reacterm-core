e2a00d5f6d020f90fa8611a8d044107d912f9d0f
Rafael Ruiz <rafaelmostoles24595@gmail.com>
2026-09-08 22:34:09 +0200

    docs: add `Option` sources documentation

--- engine/config/entry/option.go
+++ engine/config/entry/option.go
@@ -6,6 +6,7 @@ import (
 	"github.com/Rafael24595/go-reacterm-core/engine/model/winsize"
 )
 
+// Option defines a functional configuration parameter for an Entry.
 type Option func(*Entry)
 
 func defaultEntry(node screen.Node) Entry {
@@ -16,12 +17,14 @@ func defaultEntry(node screen.Node) Entry {
 	}
 }
 
+// WithLayout assigns layout options for row sizing constraints.
 func WithLayout(opts ...layer.Option[winsize.Rows]) Option {
 	return func(cfg *Entry) {
 		cfg.Opts = opts
 	}
 }
 
+// Selectable marks the entry as focusable/interactable within the screen layout.
 func Selectable() Option {
 	return func(cfg *Entry) {
 		cfg.Selectable = true
