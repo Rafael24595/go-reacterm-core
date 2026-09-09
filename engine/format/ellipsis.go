743d7b5a2cdb614ce3dda01dd8fe6c6e4c6a32bc
Rafael Ruiz <rafaelmostoles24595@gmail.com>
2026-09-09 19:50:49 +0200

    docs: add `Ellipsis` sources documentation

--- engine/format/ellipsis.go
+++ engine/format/ellipsis.go
@@ -5,11 +5,15 @@ import (
 	"github.com/Rafael24595/go-reacterm-core/engine/model/winsize"
 )
 
+// Ellipsis represents a truncation indicator symbol and its repetition count.
 type Ellipsis struct {
-	Data  string
+	// Data is the string used to indicate truncation (e.g., "...").
+	Data string
+	// Count is the number of times the Data string should be repeated.
 	Count winsize.Cols
 }
 
+// NewEllipsis returns a custom Ellipsis configuration.
 func NewEllipsis(data string, count winsize.Cols) Ellipsis {
 	return Ellipsis{
 		Data:  data,
@@ -17,6 +21,7 @@ func NewEllipsis(data string, count winsize.Cols) Ellipsis {
 	}
 }
 
+// String returns the string representation of the Ellipsis, repeating the Data string Count times.
 func (e Ellipsis) string() string {
 	if e.Count <= 0 {
 		return ""
