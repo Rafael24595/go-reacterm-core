36e2437b8bbb1bc14425147e17cb27956e553184
Rafael Ruiz <rafaelmostoles24595@gmail.com>
2026-09-13 12:46:11 +0200

    docs: add `ascii` package sources documentation

--- engine/model/ascii/ascii.go
+++ engine/model/ascii/ascii.go
@@ -1,14 +1,24 @@
 package ascii
 
 const (
+	// SPACE is the ASCII code for a space character.
 	SPACE      = 0x20
+	// ESC is the ASCII code for the escape character.
 	ESC        = 0x1b
+	// DEL is the ASCII code for the delete character.
 	DEL        = 0x7f
+	// TAB is the ASCII code for the tab character.
 	TAB        = 0x09
+	// ENTER_LF is the ASCII code for the line feed character.
 	ENTER_LF   = 0x0A
+	// ENTER_CR is the ASCII code for the carriage return character.
 	ENTER_CR   = 0x0D
+	// TILDE is the ASCII code for the tilde character.
 	TILDE      = 0x7E
+	// PLUS is the ASCII code for the plus character.
 	BACK_SPACE = 0x08
+	// MINUS is the ASCII code for the minus character.
 	PLUS       = 0x2B
+	// MINUS is the ASCII code for the minus character.
 	MINUS      = 0x2D
 )
--- engine/model/ascii/control.go
+++ engine/model/ascii/control.go
@@ -1,11 +1,18 @@
 package ascii
 
 const (
+	// CTRL_A is the ASCII code for the Ctrl+A character.
 	CTRL_A = 0x01
+	// CTRL_E is the ASCII code for the Ctrl+E character.
 	CTRL_E = 0x05
+	// CTRL_D is the ASCII code for the Ctrl+D character.
 	CTRL_D = 0x04
+	// CTRL_C is the ASCII code for the Ctrl+C character.
 	CTRL_C = 0x03
+	// CTRL_G is the ASCII code for the Ctrl+G character.
 	CTRL_G = 0x07
+	// CTRL_T is the ASCII code for the Ctrl+T character.
 	CTRL_T = 0x14
+	// CTRL_W is the ASCII code for the Ctrl+W character.
 	CTRL_W = 0x17
 )
