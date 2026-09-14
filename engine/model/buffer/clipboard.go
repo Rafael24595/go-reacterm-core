80ff07a23580e18663a74a91fd1096015a642e4b
Rafael Ruiz <rafaelmostoles24595@gmail.com>
2026-09-14 21:01:08 +0200

    refactor: improve `Clipboard` codebase

--- engine/model/buffer/clipboard.go
+++ engine/model/buffer/clipboard.go
@@ -5,21 +5,25 @@ type Clipboard struct {
 }
 
 func NewClipboard() *Clipboard {
-	return &Clipboard{
-		buffer: make([]rune, 0),
+	return &Clipboard{}
+}
+
+func (c *Clipboard) Size() uint {
+	return uint(len(c.buffer))
+}
+
+func (c *Clipboard) Buffer() []rune {
+	if len(c.buffer) == 0 {
+		return nil
 	}
+
+	result := make([]rune, len(c.buffer))
+	copy(result, c.buffer)
+	return result
 }
 
-func (b *Clipboard) Size() uint {
-	return uint(len(b.buffer))
-}
-
-func (b *Clipboard) Put(rns []rune) *Clipboard {
-	b.buffer = make([]rune, len(rns))
-	copy(b.buffer, rns)
-	return b
-}
-
-func (b *Clipboard) Buffer() []rune {
-	return b.buffer
+func (c *Clipboard) Put(rns []rune) *Clipboard {
+	c.buffer = make([]rune, len(rns))
+	copy(c.buffer, rns)
+	return c
 }
